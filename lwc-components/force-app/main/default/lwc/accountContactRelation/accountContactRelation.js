ce5c2040f70bbcfde096163cec467849eda1c470
Pankaj Sharma <59692034+pankaj-tech07@users.noreply.github.com>
2021-01-25 12:47:36 +0530

    Update accountContactRelation.js

--- lwc-components/force-app/main/default/lwc/accountContactRelation/accountContactRelation.js
+++ lwc-components/force-app/main/default/lwc/accountContactRelation/accountContactRelation.js
@@ -3,30 +3,9 @@ import { LightningElement, api, wire, track } from "lwc";
 import getRolesListValues from "@salesforce/apex/AccountContactRelationController.getRolesListValuesIntoList";
 export default class AccountContactRelation extends LightningElement {
   @api recordId;
-  @track selectedAccount = [];
-  @track mapData = [];
-  @track searchedAccount;
-  @track oldMapData = [];
-  _SelectedAccountValues = [];
   @track mapRolesData = [];
   @track selectedRoles = [];
   isLoaded = false;
-  // @wire(getAllAccount, { selectedContactId: "$recordId" })
-  // accounts({ data, error }) {
-  //   if (data) {
-  //     let mapVal = [];
-  //     data.forEach((element) => {
-  //       mapVal.push({ label: element.Name, value: element.Id });
-  //     });
-  //     this.mapData = mapVal;
-  //     this.oldMapData = [...mapVal];
-  //     console.log("this.mapData==>", this.mapData);
-  //   }
-  //   if (error) {
-  //     console.log(` Error while fetching Picklist values  ${error}`);
-  //     this.error = error;
-  //   }
-  // }
 
   @wire(getRolesListValues)
   rolesList({ data, error }) {
@@ -45,40 +24,11 @@ export default class AccountContactRelation extends LightningElement {
     }
   }
 
-  handleAccountChange(event) {
-    this.selectedAccount = event.detail.value;
-    let seletcedData = this.oldMapData.filter(
-      (obj) => this.selected.indexOf(obj.value) !== -1
-    );
-    console.log("this.selected==>", seletcedData);
-    this._SelectedAccountValues = [...seletcedData];
-    console.log("this.selected==>", event.detail.value);
-  }
-
   handleRoleChange(event) {
     this.selectedRoles = event.detail.value;
     console.log("this.selected==>", this.selectedRoles);
   }
 
-  handleSearchKeyUp(event) {
-    console.log("this.searchedAccount==>" + event.target.value);
-    if (event.target.value.length > 0) {
-      let arr = this.oldMapData.filter(
-        (obj) =>
-          obj.label.toLowerCase().indexOf(event.target.value.toLowerCase()) > -1
-      );
-      console.log("** ", arr);
-      this.mapData = [...arr, ...this._SelectedAccountValues];
-    } else {
-      this.mapData = [...this.oldMapData];
-      console.log("this.searchedAccountthis.mapData==>" + this.mapData);
-    }
-  }
-
-  // saveContactAccountRelation(event) {
-  //   const selectedAcc = event.detail;
-  //   console.log("selectedAcc==>", selectedAcc);
-  // }
   // To call the child component method to add the Contact into the list.
   saveContactAccountRelation(event) {
     this.isLoaded = true;
